4cde1d911e32f08b4e2182c89af97a0b662349c9
DianaBerte <dianaberte.go@gmail.com>
2023-02-28 17:26:57 +0100

    Installs endpoints

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import Express from "express";
 import authorsRouter from "./api/authors/index.js";
-// import listEndpoints from "express-list-endpoints";
+import listEndpoints from "express-list-endpoints";
 
 const server = Express();
 server.use(Express.json());
@@ -9,6 +9,6 @@ const port = 3001;
 server.use("/authors", authorsRouter);
 
 server.listen(port, () => {
-  //   console.table(listEndpoints(server));
+  console.table(listEndpoints(server));
   console.log(`The server is running on port ${port}`);
 });
