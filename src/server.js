d6f29cd321a276ba81f09b2f76575ef737f12daa
DianaBerte <dianaberte.go@gmail.com>
2023-02-28 17:14:00 +0100

    Adds server.use

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import authorsRouter from "./api/authors/index.js";
 // import listEndpoints from "express-list-endpoints";
 
 const server = Express();
+server.use(Express.json());
 const port = 3001;
 
 server.use("/authors", authorsRouter);
