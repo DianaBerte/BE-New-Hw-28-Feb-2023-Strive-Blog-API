fdc63e92df43db374bc86fa6d357ebc2ba234583
DianaBerte <dianaberte.go@gmail.com>
2023-03-01 16:42:19 +0100

    Installed cors

--- src/api/blog_posts/index.js
+++ src/api/blog_posts/index.js
@@ -1,17 +1,49 @@
 import Express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import uniqid from "uniqid";
 
 const blogPostsRouter = Express.Router();
 
+const blogpostsJSONPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "blogPosts.json"
+);
+
+const getBlogPosts = () => JSON.parse(fs.readFileSync(blogpostsJSONPath));
+const writeBlogPosts = (blogPostsArray) =>
+  fs.writeFileSync(blogpostsJSONPath, JSON.stringify(blogPostsArray));
+
+//1.
 blogPostsRouter.post("/", (req, res) => {
-  res.send();
+  const newBlogPost = {
+    ...req.body,
+    id: uniqid(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  const blogPostsArray = getBlogPosts();
+  blogPostsArray.push(newBlogPost);
+  writeBlogPosts(blogPostsArray);
+
+  res.status(201).send({ id: newBlogPost.id });
 });
 
-blogPostsRouter.get("/", (req, res) => {});
+//2.
+blogPostsRouter.get("/", (req, res) => {
+  const blogPosts = getBlogPosts();
+  res.send(blogPosts);
+});
 
+//3.
 blogPostsRouter.get("/:blogpostId", (req, res) => {});
 
+//4.
 blogPostsRouter.put("/:blogpostId", (req, res) => {});
 
+//5.
 blogPostsRouter.delete("/:blogpostId", (req, res) => {});
 
 export default blogPostsRouter;
--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import Express from "express";
 import authorsRouter from "./api/authors/index.js";
 import listEndpoints from "express-list-endpoints";
 import blogPostsRouter from "./api/blog_posts/index.js";
+import cors from "cors";
 
 const server = Express();
-const port = 3001;
+const port = 3004;
 
 server.use(Express.json());
+server.use(cors());
 
 server.use("/authors", authorsRouter);
 server.use("/blogposts", blogPostsRouter);
