63baf50c2ec04e5d19fee17be9c44e563bd62cad
DianaBerte <dianaberte.go@gmail.com>
2023-03-02 16:36:04 +0100

    Image saved in public/img/blogPosts

--- src/files/index.js
+++ src/files/index.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 import Express from "express";
+import { saveBlogPostsCovers } from "../lib/fs-tools.js";
 
 //POST /blogPosts/:id/uploadCover, uploads a picture (save as idOfTheBlogPost.jpg in the public/img/blogPosts folder) for the blog post specified by the id. Store the newly created URL into the corresponding post in blogPosts.json
 
@@ -8,6 +9,7 @@ const filesRouter = Express.Router()
 filesRouter.post("/single", multer().single("cover"), async (req, res, next) => {
     try {
         console.log('FILE:', req.file)
+        await saveBlogPostsCovers(req.file.originalname, req.file.buffer)
         res.send({ message: "Hello, file uploaded!" })
     } catch (error) {
         next(error)
--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -2,11 +2,15 @@ import fs from "fs-extra";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
-const { readJSON, writeJSON } = fs;
+const { readJSON, writeJSON, writeFile } = fs;
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
 const blogpostsJSONPath = join(dataFolderPath, "blogposts.jSON")
 // console.log("DATA FOLDER:", dataFolderPath)
+const blogPostsPublicFolderPath = join(process.cwd(), "./public/img/blogPosts")
+
 export const getBlogPosts = () => readJSON(blogpostsJSONPath);
 export const writeBlogPosts = blogPostsArray => writeJSON(blogpostsJSONPath, blogPostsArray);
+
+export const saveBlogPostsCovers = (filename, fileContentAsBuffer) => writeFile(join(blogPostsPublicFolderPath, filename), fileContentAsBuffer)
 
