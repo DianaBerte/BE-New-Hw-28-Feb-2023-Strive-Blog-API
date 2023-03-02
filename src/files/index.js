3c2b1758ed98f318d6a04ccfe74b0e8b2c6ddb3d
DianaBerte <dianaberte.go@gmail.com>
2023-03-02 16:46:48 +0100

    Uploads and saves pic as idOfTheBlogPost.jpg in public/img/blogPosts folder

--- src/files/index.js
+++ src/files/index.js
@@ -1,15 +1,18 @@
 import multer from "multer";
 import Express from "express";
 import { saveBlogPostsCovers } from "../lib/fs-tools.js";
+import { extname } from 'path';
 
 //POST /blogPosts/:id/uploadCover, uploads a picture (save as idOfTheBlogPost.jpg in the public/img/blogPosts folder) for the blog post specified by the id. Store the newly created URL into the corresponding post in blogPosts.json
 
 const filesRouter = Express.Router()
 
-filesRouter.post("/single", multer().single("cover"), async (req, res, next) => {
+filesRouter.post("/:id/single", multer().single("cover"), async (req, res, next) => {
     try {
-        console.log('FILE:', req.file)
-        await saveBlogPostsCovers(req.file.originalname, req.file.buffer)
+        console.log('FILE:', req.file);
+        const originalFileExtension = extname(req.file.originalname);
+        const coverName = req.params.id + originalFileExtension;
+        await saveBlogPostsCovers(coverName, req.file.buffer)
         res.send({ message: "Hello, file uploaded!" })
     } catch (error) {
         next(error)
