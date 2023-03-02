acbac4172de4e321faccd374895a3319c8490442
DianaBerte <dianaberte.go@gmail.com>
2023-03-02 13:40:34 +0100

    Moves dataFolderPath to folder 'data'

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -4,6 +4,8 @@ import { dirname, join } from "path";
 
 const { readJSON, writeJSON } = fs;
 
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
+
 export const getBlogPosts = () => readJSON(blogpostsJSONPath);
 export const writeBlogPosts = (blogPostsArray) =>
   writeJSON(blogpostsJSONPath, blogPostsArray);
