9f4d4a1748e283ea32d84db1b1002c82e07dd84c
DianaBerte <dianaberte.go@gmail.com>
2023-04-05 15:57:20 +0200

    Creates access token & verify access token

---
+++ src/lib/auth/tools.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+//input: payload; output: promise resolving into token
+export const createAccessToken = payload =>
+    new Promise((resolve, reject) =>
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1 week" }, (err, token) => {
+            if (err) reject(err)
+            else resolve(token)
+        }))
+
+//input: token; output: promise resolving into original payload
+export const verifyAccessToken = token =>
+    new Promise((resolve, reject) =>
+        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+            if (err) reject(err)
+            else resolve(payload)
+        })
+    )
